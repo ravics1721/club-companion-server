82b4e933c25bad80db0de4f3a33aeadfb69c28ab
Ravi Bharti <ravics1718@gmail.com>
2021-08-05 01:38:44 +0530

    created prisma schema & successfully created user �

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const Prisma = require("@prisma/client");
 const authRoutes = express.Router();
 
-authRoutes.route("/auth")
-    .get((req, res) => {
-        res.json({
-            message:"Auth routes",
-        })
+const prisma = new Prisma.PrismaClient();
+
+authRoutes.post("/signup",async (req, res) => {
+    const user = await prisma.user.create({
+        data: {
+            ...req.body
+        }
+    });
+    res.json({
+        message: "completed",
+        data: user
     })
+})
 
 module.exports = authRoutes;
