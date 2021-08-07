3ed1ce0f9bcf2c321526e78dc54ca1a0e476733b
Ravi Bharti <ravics1718@gmail.com>
2021-08-08 01:35:34 +0530

    added prettier precommit hooks

--- middlewares/app.js
+++ middlewares/app.js
@@ -3,10 +3,10 @@ const logger = require("morgan");
 const cors = require("cors");
 
 const main = (app) => {
-    app.use(cors());
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-}
+  app.use(cors());
+  app.use(logger("dev"));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+};
 
-module.exports = main
+module.exports = main;
