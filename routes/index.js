5b03d57aa4de1e8f48d89996e289c52badcec287
Ravi Bharti <ravics1718@gmail.com>
2021-08-09 03:24:06 +0530

    Added routes 🛣

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
-const express = require('express');
-const authRoutes = require('./auth');
-const usersRoutes = require('./user');
-const commonRoutes = require('./common');
+const express = require("express");
+const authRoutes = require("./auth");
+const usersRoutes = require("./user");
+const commonRoutes = require("./common");
 
 const router = {
-    init: (app) => {
-        app.use("/", commonRoutes);
-    }
-}
+  init: (app) => {
+    app.use("/", commonRoutes);
+    app.use("/api", authRoutes);
+    app.use("/api", usersRoutes);
+  },
+};
 module.exports = router;
