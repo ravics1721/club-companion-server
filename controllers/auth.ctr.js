6b9bbda0dbcb35eec346e30763ad39b034c6b29d
Ravi Bharti <ravics1718@gmail.com>
2021-08-09 03:16:06 +0530

    Created Auth Request validator & controller

--- controllers/auth.ctr.js
+++ controllers/auth.ctr.js
@@ -1,8 +1,36 @@
-const joi = require("joi");
-const auth = {
-  signup: (req, res, next) => {
-    const { name, email, username, password } = req.body;
-    //validate request
+const authSchema = require("../schema/auth");
+const authCtr = {
+  signup: async (req, res, next) => {
+    try {
+      const value = await authSchema.signup.validate(req.body);
+      if (value.error) {
+        throw value.error.message;
+      }
+      return next();
+    } catch (e) {
+      return res.status(400).json({
+        status: 400,
+        message: null,
+        error: e,
+        data: "",
+      });
+    }
+  },
+  login: async (req, res, next) => {
+    try {
+      const value = await authSchema.login.validate(req.body);
+      if (value.error) {
+        throw value.error.message;
+      }
+      return next();
+    } catch (e) {
+      return res.status(400).json({
+        status: 400,
+        message: null,
+        error: e,
+        data: "",
+      });
+    }
   },
 };
 
